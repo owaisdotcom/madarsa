45008082f8d66f6732b7e66a63295a81065150a7
Muhammad Owais Ansari <owais.ansari@octdaily.com>
2025-12-16 22:32:32 +0500

    fix

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:5000/api';
-// const API_URL = 'https://madarsa-api.vercel.app/api';
+// const API_URL = 'http://localhost:5000/api';
+const API_URL = 'https://madarsa-api.vercel.app/api';
 
 const api = axios.create({
   baseURL: API_URL,
